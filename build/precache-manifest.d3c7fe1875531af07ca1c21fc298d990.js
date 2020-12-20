1f5ccf1ec692b10e182d19cabddb447205365e9d
haszari <haszari@cartoonbeats.com>
2020-12-20 14:18:08 +1300

    add ignored build files to gh-pages

---
+++ build/precache-manifest.d3c7fe1875531af07ca1c21fc298d990.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ebab01e5ff0946f9f0b46a200270dd4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "58998693572232ff3bbb",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "fb427c66390447cf9893",
+    "url": "/static/js/2.0a7b9b7c.chunk.js"
+  },
+  {
+    "revision": "68ba00d5eb083746d913686923a3d904",
+    "url": "/static/js/2.0a7b9b7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58998693572232ff3bbb",
+    "url": "/static/js/main.9ee2f64f.chunk.js"
+  },
+  {
+    "revision": "fa41381c143b6e5a84b7",
+    "url": "/static/js/runtime-main.70ea06df.js"
+  }
+]);
