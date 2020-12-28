0ad3ea9d22fc41555f9ebb3b42ced8310247c5d9
haszari <haszari@cartoonbeats.com>
2020-12-28 16:44:47 +1300

    revert "custom" theme until I figure out how to do that nicely

--- src/theme.js
+++ src/theme.js
@@ -1,22 +1,25 @@
-import { orange } from "@material-ui/core/colors";
 import { createMuiTheme } from "@material-ui/core/styles";
 
-// A custom theme for this app
+// A custom theme for this app…
+// …or not, need to better understand how to define a custom theme from 1-3 colours.
 const theme = createMuiTheme({
-  palette: {
-    primary: {
-      main: "#ff0022",
-    },
-    secondary: {
-      main: "#22ff00",
-    },
-    error: {
-      main: orange.A400,
-    },
-    background: {
-      default: "#aaa",
-    },
-  },
+  // palette: {
+  //   primary: {
+  //     main: "#ff0022",
+  //   },
+  //   secondary: {
+  //     main: "#22ff00",
+  //   },
+  //   error: {
+  //     main: orange.A400,
+  //   },
+  //   background: {
+  //     default: "#cga",
+  //   },
+  //   action: {
+  //     selected: "green",
+  //   },
+  // },
 });
 
 export default theme;
