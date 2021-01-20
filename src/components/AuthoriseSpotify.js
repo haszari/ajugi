df21e9b331074b577c17fe02a3088394ccea2122
haszari <haszari@cartoonbeats.com>
2021-01-20 23:04:51 +1300

    adjust permissions so can trigger playback of songs/albums: - specify spotify permissions/scopes, including playback permission

--- src/components/AuthoriseSpotify.js
+++ src/components/AuthoriseSpotify.js
@@ -1,23 +1,18 @@
-import React from 'react';
+import React from "react";
 
-import spotifyApp from '../config/spotify.js';
+import spotifyApp from "../config/spotify.js";
 
 function AuthoriseSpotify() {
-  const spotifyAuthUrl = 'https://accounts.spotify.com/authorize?response_type=token'
-    + `&client_id=${ spotifyApp.clientId }`
-    + `&redirect_uri=${ spotifyApp.redirectUrl }`
-    // + `&state=${ randomString }`
-  ;
-
+  const spotifyAuthUrl =
+    "https://accounts.spotify.com/authorize?response_type=token" +
+    `&client_id=${spotifyApp.clientId}` +
+    `&redirect_uri=${spotifyApp.redirectUrl}` +
+    `&scope=${encodeURIComponent(spotifyApp.scopes)}`;
+  // + `&state=${ randomString }`
   return (
     <>
-      <p>
-        Click button to authorise with Spotify.
-      </p>
-      <a
-        href={ spotifyAuthUrl }
-        rel="noopener noreferrer"
-      >
+      <p>Click button to authorise with Spotify.</p>
+      <a href={spotifyAuthUrl} rel="noopener noreferrer">
         Authorise
       </a>
     </>
